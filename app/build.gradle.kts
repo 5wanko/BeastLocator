0cf2de83d8f62969004f2e199d351f5e01b72c3f
きるら <57588049+KiRura@users.noreply.github.com>
2026-03-25 18:30:04 +0900

    依存関係の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -374,10 +374,10 @@ tasks.named("preBuild").configure {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.activity:activity-ktx:1.13.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 }
